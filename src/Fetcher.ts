5a4092004d62ef49050c31a6f03aab611fdde1bd
Lucas Leonardo <lucasleonardo@protonmail.com>
2020-08-13 15:21:12 -0300

    fetcher: added file

---
+++ src/Fetcher.ts
@@ -0,0 +1,19 @@
+import { Image } from "./Image";
+import axios from "axios";
+
+export const getAlbum = async (al: string) => {
+  let albums: Image[] = [];
+  axios
+    .get("/api/albums?album=" + al)
+    .then((resp) => {
+      for (const al of resp.data.albums) {
+        albums.push({ title: al.album, author: al.artist, url: al.cover });
+      }
+    })
+    .catch((err) => {
+      alert(err);
+      console.log(err);
+    });
+
+  return albums;
+};

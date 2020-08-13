7b6f1d3a5a5a4857be215a151b092355fb56b1c0
Lucas Leonardo <lucasleonardo@protonmail.com>
2020-08-13 15:30:43 -0300

    api: use mock instead of actual api

--- api/main.py
+++ api/main.py
@@ -3,9 +3,9 @@ from fetcher import LastFM
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/api", methods=['GET'])
 def index():
-    return "index page"
+    return { "albums" : [{"album" : "white pony", "artist" : "deftones", "cover" : "https://fanart.tv/fanart/music/7527f6c2-d762-4b88-b5e2-9244f1e34c46/albumcover/white-pony-57f3877012b98.jpg"}]  }
 
 
 @app.route("/api/albums", methods=['GET'])
--- src/Fetcher.ts
+++ src/Fetcher.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 export const getAlbum = async (al: string) => {
   let albums: Image[] = [];
   axios
-    .get("/api/albums?album=" + al)
+    .get("/api")
     .then((resp) => {
       for (const al of resp.data.albums) {
         albums.push({ title: al.album, author: al.artist, url: al.cover });

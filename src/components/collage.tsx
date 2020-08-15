518d6a42059981e76b9e39410eb6187a9e5a7afd
Lucas Leonardo <lucasleonardo@protonmail.com>
2020-08-15 16:00:52 -0300

    collage: replace dropdown with list

--- src/components/collage.tsx
+++ src/components/collage.tsx
@@ -1,8 +1,6 @@
 import React from "react";
-import { Dropdown } from "semantic-ui-react";
 
 import { Image } from "./image";
-
 import "./collage.css";
 
 interface Props {
@@ -11,16 +9,11 @@ interface Props {
 }
 
 export const Collage: React.FC<Props> = ({ images, titleVisible }) => {
-  const results = images.map((img) => {
-    const val = img.author + " - " + img.title;
-    return { key: img.url, text: val, value: val, image: { avatar: false, src: img.url } };
-  });
-  return <Dropdown placeholder="Click here to see results!" fluid selection options={results} />;
-  /* return ( */
-  /*   <div className="collage-container"> */
-  /*     {images.map((img) => ( */
-  /*       <Image key={img.url} image={img} showTitle={titleVisible} /> */
-  /*     ))} */
-  /*   </div> */
-  /* ); */
+  return (
+    <div className="collage-container">
+      {images.map((img) => (
+        <Image key={img.url} image={img} showTitle={true} />
+      ))}
+    </div>
+  );
 };

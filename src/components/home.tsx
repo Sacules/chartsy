ee85769c8d64ed6e619f106d30b13cbc871b7f1a
Lucas Leonardo <lucasleonardo@protonmail.com>
2020-08-19 23:06:30 -0300

    home: remove unused import

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { Grid, Button } from "semantic-ui-react";
 
 import { onResults } from "./results";

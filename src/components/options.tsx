23042041c937d076f258fb9b9e036be04f405b37
Lucas Leonardo <lucasleonardo@protonmail.com>
2020-08-22 12:17:30 -0300

    options: add more buttons

--- src/components/options.tsx
+++ src/components/options.tsx
@@ -1,6 +1,33 @@
 import React from "react";
-import { Button } from "semantic-ui-react";
+import { Grid, Button } from "semantic-ui-react";
 
-export const Save: React.FC = () => {
-  return <Button>UwU</Button>;
+interface Props {
+  cols: number;
+  setCols: (cols: number) => void;
+}
+
+export const Save: React.FC<Props> = ({ cols, setCols }) => {
+  return (
+    <Grid>
+      <Grid.Row>
+        <Grid.Column>
+          <p>
+            <b>Columns </b>
+            {cols}
+          </p>
+          <Button content="-" onClick={() => setCols(cols - 1)} />
+          <Button content="+" onClick={() => setCols(cols + 1)} />
+        </Grid.Column>
+      </Grid.Row>
+
+      <Grid.Row>
+        <Button
+          content="Save to PNG"
+          onClick={() => {
+            console.log("saving...");
+          }}
+        />
+      </Grid.Row>
+    </Grid>
+  );
 };

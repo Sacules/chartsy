0c924e8fdc721e283093fadf9edfb77643fbce14
Lucas Leonardo <lucasleonardo@protonmail.com>
2020-08-28 19:58:03 -0300

    fetcher: fix api

--- src/components/fetcher.ts
+++ src/components/fetcher.ts
@@ -3,13 +3,7 @@ import axios from "axios";
 
 export const getAlbum = async (al: string) => {
   return axios.get("/api/albums?album=" + al.replace(" ", "+")).then((resp) => {
-    let albums: Image[] = [];
-
-    for (const al of resp.data.albums) {
-      albums.push({ title: al.album, author: al.artist, url: al.cover });
-    }
-
-    return albums;
+    return resp.data.albums;
   });
 };
 

5cc399d666351705f4ec1abf67952b1a2572d3e5
Lucas Leonardo <lucasleonardo@protonmail.com>
2021-05-30 00:09:42 -0300

    allow CORS

--- src/services/index.ts
+++ src/services/index.ts
@@ -4,14 +4,22 @@ import { Image } from "../common/entities";
 
 const APIURL = "https://api.chartsy.net";
 
+const http = axios.create({
+  baseURL: APIURL,
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  },
+});
+
 export const getAlbum = async (al: string) => {
-  return axios.get(APIURL + "/albums?search=" + al.replace(" ", "+")).then((resp) => {
+  return http.get("/albums?search=" + al.replace(" ", "+")).then((resp) => {
     return resp.data.albums;
   });
 };
 
 export const getGame = async (game: string) => {
-  return axios.get(APIURL + "/games?search=" + game.replace(" ", "+")).then((resp) => {
+  return http.get("/games?search=" + game.replace(" ", "+")).then((resp) => {
     let games: Image[] = [];
 
     for (const g of resp.data.games) {
@@ -23,7 +31,7 @@ export const getGame = async (game: string) => {
 };
 
 export const getMovie = async (movie: string) => {
-  return axios.get(APIURL + "/movies?search=" + movie.replace(" ", "+")).then((resp) => {
+  return http.get("/movies?search=" + movie.replace(" ", "+")).then((resp) => {
     let movies: Image[] = [];
 
     for (const g of resp.data.movies) {
@@ -35,7 +43,7 @@ export const getMovie = async (movie: string) => {
 };
 
 export const getSeries = async (show: string) => {
-  return axios.get(APIURL + "/series?search=" + show.replace(" ", "+")).then((resp) => {
+  return http.get("/series?search=" + show.replace(" ", "+")).then((resp) => {
     let series: Image[] = [];
 
     for (const g of resp.data.series) {

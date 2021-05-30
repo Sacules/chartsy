215400e8fb4c7a1c9083197f9df4a3cd6c38d0ba
Lucas Leonardo <lucasleonardo@protonmail.com>
2021-05-30 00:57:39 -0300

    add missing method

--- api/server.go
+++ api/server.go
@@ -36,7 +36,7 @@ func setupRouter() *chi.Mux {
 	r.Use(
 		cors.Handler(cors.Options{
 			AllowedOrigins:   []string{"https://*", "http://*"},
-			AllowedMethods:   []string{"GET"},
+			AllowedMethods:   []string{"GET", "OPTIONS"},
 			AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 			AllowCredentials: false,
 			MaxAge:           300, // Maximum value not ignored by any of major browsers

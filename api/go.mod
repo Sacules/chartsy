4b7b9141344d831e2750c6af3bf0a0ee1c22d793
Lucas Leonardo <lucasleonardo@protonmail.com>
2021-05-30 00:36:30 -0300

    add CORS to api

--- api/go.mod
+++ api/go.mod
@@ -5,6 +5,7 @@ go 1.15
 require (
 	github.com/asdine/storm v2.1.2+incompatible
 	github.com/go-chi/chi v4.1.2+incompatible
+	github.com/go-chi/cors v1.2.0 // indirect
 	github.com/go-chi/render v1.0.1
 	gitlab.com/sacules/log v0.1.0
 	go.etcd.io/bbolt v1.3.5 // indirect
--- api/server.go
+++ api/server.go
@@ -6,6 +6,7 @@ import (
 	"github.com/asdine/storm"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/cors"
 	"github.com/go-chi/render"
 	"gitlab.com/sacules/log"
 )
@@ -33,6 +34,13 @@ func setupRouter() *chi.Mux {
 	r := chi.NewRouter()
 
 	r.Use(
+		cors.Handler(cors.Options{
+			AllowedOrigins:   []string{"https://*", "http://*"},
+			AllowedMethods:   []string{"GET"},
+			AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+			AllowCredentials: false,
+			MaxAge:           300, // Maximum value not ignored by any of major browsers
+		}),
 		render.SetContentType(render.ContentTypeJSON),
 		middleware.Recoverer,
 		Logger,

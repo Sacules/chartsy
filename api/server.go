e1a5586ac6121fdadd5fe4bb27275c193ace204b
Lucas Leonardo <lucasleonardo@protonmail.com>
2021-05-30 12:23:58 -0300

    add missing header

--- api/server.go
+++ api/server.go
@@ -27,7 +27,7 @@ func newServer() *server {
 func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization, cache-control")
+	w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization, cache-control, access-control-allow-origin")
 	if r.Method == "OPTIONS" {
 		return
 	}

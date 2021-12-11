cbc8085c3de96d38acfae96cb0ed86642b97da6c
Lucas Leonardo <lucasleonardo@protonmail.com>
2021-12-11 14:56:26 -0300

    misc: update tailwind to v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,15 @@
 module.exports = {
-  mode: "jit",
-  purge: [
+  content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {
       gridTemplateColumns: {
         layout: "250px 1fr",
       },
       gridTemplateRows: {
-        layout: "3rem 10rem 1fr",
+        layout: "3rem 1fr",
       },
     },
   },

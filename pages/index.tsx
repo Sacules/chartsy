71dce946f5c8d1d3a4c1f51ead7acf61e825633b
Lucas Leonardo <lucasleonardo@protonmail.com>
2021-12-13 16:54:03 -0300

    misc: add favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,6 +15,19 @@ import { Chart } from "@components/Chart";
 import { Search } from "@components/Search";
 import { Config } from "@components/Config";
 
+const CustomHead: React.FC = () => (
+  <Head>
+    <title>Chartsy</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <link rel="manifest" href="/site.webmanifest" />
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+    <meta name="msapplication-TileColor" content="#7ecce0" />
+    <meta name="theme-color" content="#ffffff" />
+  </Head>
+);
+
 interface Props {
   showConfig: boolean;
 }
@@ -61,11 +74,8 @@ export default function Home() {
 
   return (
     <>
-      <Head>
-        <title>Chartsy</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className="min-h-screen">
+      <CustomHead />
+      <main className="min-h-screen md:flex">
         <RefNav ref={ref} />
         <ConfigProvider>
           <ChartProvider>

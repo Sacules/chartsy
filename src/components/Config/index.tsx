18a6fc63a507b94d92e67c90ac1e0b4d8e5b0a8f
Lucas Leonardo <lucasleonardo@protonmail.com>
2022-11-25 19:39:13 -0300

    Config: rework ranges with react-aria

--- src/components/Config/index.tsx
+++ src/components/Config/index.tsx
@@ -1,35 +1,83 @@
 import { useConfig } from "@contexts/ConfigContext";
+import { useRef } from "react";
+import {
+  mergeProps,
+  useFocusRing,
+  useNumberFormatter,
+  useSlider,
+  useSliderThumb,
+  VisuallyHidden,
+} from "react-aria";
+import { useSliderState } from "react-stately";
 
 interface SliderProps {
   title: string;
   value: number;
-  min: number;
-  max: number;
-  dispatch: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  minValue: number;
+  maxValue: number;
+  onChange: (val: number) => void;
 }
+
+const Thumb = ({ state, trackRef, index }: any) => {
+  const inputRef = useRef(null);
+  const { thumbProps, inputProps, isDragging } = useSliderThumb(
+    { index, trackRef, inputRef },
+    state
+  );
+  const { focusProps, isFocusVisible } = useFocusRing();
+
+  return (
+    <div
+      {...thumbProps}
+      className={`w-5 h-5 bg-slate-600 top-1/2 rounded-full ${
+        isFocusVisible ? "bg-orange-500" : ""
+      }`}
+    >
+      <VisuallyHidden>
+        <input ref={inputRef} {...mergeProps(inputProps, focusProps)} />
+      </VisuallyHidden>
+    </div>
+  );
+};
 
 const Slider: React.FC<SliderProps> = ({
   title,
   value,
-  min,
-  max,
-  dispatch,
-}) => (
-  <div>
-    <div className="flex justify-between">
-      <p>{title}</p>
-      <p>{value}</p>
+  minValue,
+  maxValue,
+  onChange,
+}) => {
+  const trackRef = useRef(null);
+  const numberFormatter = useNumberFormatter({ style: "decimal" });
+  const state = useSliderState({
+    numberFormatter,
+    value,
+    minValue,
+    maxValue,
+    onChange,
+  });
+  const { groupProps, trackProps, labelProps, outputProps } = useSlider(
+    { label: title },
+    state,
+    trackRef
+  );
+
+  return (
+    <div {...groupProps} className="h-8 flex flex-col gap-2">
+      <div className="flex justify-between">
+        <label {...labelProps}>{title}</label>
+        <output {...outputProps}>{value}</output>
+      </div>
+      <div
+        {...trackProps}
+        ref={trackRef}
+        className="before:content-[attr(x)] before:block before:absolute before:bg-slate-600 before:h-1 before:w-full before:top-1/2 before:-translate-y-1/2"
+      >
+        <Thumb index={0} state={state} trackRef={trackRef} />
+      </div>
     </div>
-    <input
-      className="w-full"
-      type="range"
-      min={min}
-      max={max}
-      value={value}
-      onChange={dispatch}
-    />
-  </div>
-);
+  );
+};
 interface RadioProps {
   title: string;
   dispatch: () => void;
@@ -65,36 +113,36 @@ export const Config: React.FC<Props> = ({ show }) => {
 
   return (
     <aside
-      className={`z-10 shadow overflow-hidden md:max-w-0 md:max-h-full transition-all duration-300 ${
+      className={`z-10 border-b border border-slate-200 overflow-hidden md:max-w-0 md:max-h-full transition-all duration-300 ${
         show ? "max-h-screen" : "max-h-0"
       }`}
     >
-      <div className="p-4 bg-white flex flex-col gap-2 min-h-max md:min-w-max">
+      <div className="p-4 bg-white flex flex-col gap-6 min-h-max md:min-w-max">
         <Slider
           title="Rows"
           value={rows}
-          min={1}
-          max={10}
-          dispatch={(e) =>
-            dispatch({ type: "update", field: "rows", value: e.target.value })
+          minValue={1}
+          maxValue={10}
+          onChange={(value) =>
+            dispatch({ type: "update", field: "rows", value })
           }
         />
         <Slider
           title="Columns"
           value={cols}
-          min={1}
-          max={10}
-          dispatch={(e) =>
-            dispatch({ type: "update", field: "cols", value: e.target.value })
+          minValue={1}
+          maxValue={10}
+          onChange={(value) =>
+            dispatch({ type: "update", field: "cols", value })
           }
         />
         <Slider
           title="Padding"
           value={pad}
-          min={0}
-          max={8}
-          dispatch={(e) =>
-            dispatch({ type: "update", field: "pad", value: e.target.value })
+          minValue={0}
+          maxValue={8}
+          onChange={(value) =>
+            dispatch({ type: "update", field: "pad", value })
           }
         />
         <Radio

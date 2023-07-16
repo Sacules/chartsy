193a374f67d38a28da7710e2c3c383ccc693e96f
Lucas Leonardo <lucasleonardo@protonmail.com>
2023-07-16 03:37:35 -0300

    web: add mysql driver

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
 
 type application struct {
 	infoLog  *log.Logger
@@ -14,10 +30,18 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:secret@/chartsy?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "\033[1;32mINFO\033[0m\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "\033[1;31mERROR\033[0m\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
 
 	app := &application{
 		infoLog,
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module gitlab.com/sacules/chartsy
 go 1.20
 
 require github.com/go-chi/chi/v5 v5.0.8
+
+require github.com/go-sql-driver/mysql v1.7.1 // indirect

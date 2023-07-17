7974dc5c43cf9cb1eeb5db4f1042a3b0583092d5
Lucas Leonardo <lucasleonardo@protonmail.com>
2023-07-17 04:10:08 -0300

    misc: add endpoint for updating chart

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -15,6 +17,28 @@ func (app *application) routes() *chi.Mux {
 
 	r.Handle("/static/*", http.StripPrefix("/static", fileServer))
 	r.Get("/", app.home)
+	r.Route("/charts", func(r chi.Router) {
+		r.Patch("/settings", func(w http.ResponseWriter, r *http.Request) {
+			err := r.ParseForm()
+			if err != nil {
+				app.errorLog.Println(err)
+				app.clientError(w, http.StatusBadRequest)
+				return
+			}
+
+			title := r.Form.Get("title")
+			id, _ := strconv.Atoi(r.Form.Get("id"))
+			columns, _ := strconv.Atoi(r.Form.Get("columns"))
+			rows, _ := strconv.Atoi(r.Form.Get("rows"))
+			spacing, _ := strconv.Atoi(r.Form.Get("spacing"))
+			margins, _ := strconv.Atoi(r.Form.Get("margins"))
+			imgsHeight, _ := strconv.Atoi(r.Form.Get("imgs-height"))
+
+			fmt.Fprintln(w, "received something!")
+			fmt.Fprintln(w, "title", title, "id", id, "columns", columns, "rows", rows, "spacing", spacing, "margins", margins, "imgs-height", imgsHeight)
+		})
+	})
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		app.notFound(w)
 	})

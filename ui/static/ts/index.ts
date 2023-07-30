c23fce3a898e1ac14be1c83e0cd67adfdce87df5
Lucas Leonardo <lucasleonardo@protonmail.com>
2023-07-30 17:30:01 -0300

    misc: finally, download images again

--- ui/static/ts/index.ts
+++ ui/static/ts/index.ts
@@ -1,3 +1,4 @@
+import * as htmlToImage from "html-to-image";
 import Sortable from "sortablejs";
 import yabbcode from "ya-bbcode";
 import htmx from "htmx.org";
@@ -5,27 +6,43 @@ import htmx from "htmx.org";
 const parser = new yabbcode();
 
 export function bbcode(s: string) {
-  return parser.parse(s);
+	return parser.parse(s);
+}
+
+export function downloadChart() {
+	const chart = document.getElementById("chart") as HTMLDivElement;
+	htmlToImage.toPng(chart).then(function(dataUrl) {
+		const link = document.createElement("a");
+		// Some older browsers may not fully support downloading
+		if (typeof link.download !== "string") {
+			window.open(dataUrl);
+			return;
+		}
+
+		link.href = dataUrl;
+		link.download = "chartsy.png";
+		link.click();
+	});
 }
 
 (window as any).bbcode = bbcode;
+(window as any).downloadChart = downloadChart;
 
-htmx.onLoad(function (content) {
-  var sortables = content.querySelectorAll(".sortable");
+htmx.onLoad(function(content) {
+	var sortables = content.querySelectorAll(".sortable");
 
-  for (var i = 0; i < sortables.length; i++) {
-    var sortable = sortables[i];
+	for (var i = 0; i < sortables.length; i++) {
+		var sortable = sortables[i];
 
-    new Sortable(sortable as HTMLElement, {
-      animation: 300,
-      ghostClass: "ghost-album",
-      invertSwap: true,
-    });
-  }
+		new Sortable(sortable as HTMLElement, {
+			animation: 300,
+			ghostClass: "ghost-album",
+			invertSwap: true,
+		});
+	}
 });
 
 /*
-import html2canvas from 'html2canvas';
 import Alpine from 'alpinejs';
 var panzoom = require('panzoom')
 
@@ -34,9 +51,6 @@ window.Alpine = Alpine;
 var element = document.getElementById('scene')
 panzoom(element)
 
-html2canvas(document.body).then(function(canvas) {
-	document.body.appendChild(canvas);
-});
 
 Alpine.start();
 */

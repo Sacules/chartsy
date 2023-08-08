3970428837b1172d900f944eb05c33f5c0c1a5a0
Lucas Leonardo <lucasleonardo@protonmail.com>
2023-08-07 21:13:11 -0300

    ui/*: change from templates to web component

--- ui/static/ts/components/input.ts
+++ ui/static/ts/components/input.ts
@@ -1,5 +1,5 @@
-import { html } from "lit";
-import { customElement, property } from "lit/decorators.js";
+import { PropertyValues, html } from "lit";
+import { customElement, property, query } from "lit/decorators.js";
 import { BaseElement } from "./base";
 import "./icon";
 
@@ -11,6 +11,37 @@ export class InputNumeric extends BaseElement {
   @property() min = 1;
   @property() max = 10;
 
+  @query("#minus-button") minusButton!: HTMLButtonElement;
+  @query("#plus-button") plusButton!: HTMLButtonElement;
+
+  decrement() {
+    this.value--;
+    if (this.value <= this.min) {
+      this.minusButton.setAttribute("disabled", "true");
+    }
+
+    if (this.value < this.max) {
+      this.plusButton.removeAttribute("disabled");
+    }
+  }
+
+  increment() {
+    this.value++;
+    if (this.value >= this.max) {
+      this.plusButton.setAttribute("disabled", "true");
+    }
+
+    if (this.value > this.min) {
+      this.minusButton.removeAttribute("disabled");
+    }
+  }
+
+  protected override updated(changedProperties: PropertyValues<this>) {
+    if (changedProperties.has("value")) {
+      console.log(this.value);
+    }
+  }
+
   override render() {
     return html`
       <div
@@ -18,12 +49,14 @@ export class InputNumeric extends BaseElement {
         class="flex flex-col gap-1 md:flex-row md:justify-between items-center"
       >
         <label for="${this.id}">
-          <slot name="label"></slot>
+          <slot></slot>
         </label>
         <div class="flex items-center gap-4">
           <button
+            id="minus-button"
             type="button"
             class="w-8 h-8 md:w-6 md:h-6 disabled:text-slate-600 disabled:cursor-not-allowed"
+            @click="${this.decrement}"
           >
             <icon-minus></icon-minus>
           </button>
@@ -31,8 +64,10 @@ export class InputNumeric extends BaseElement {
             ${this.value}
           </output>
           <button
+            id="plus-button"
             type="button"
             class="w-8 h-8 md:w-6 md:h-6 disabled:text-slate-600 disabled:cursor-not-allowed"
+            @click="${this.increment}"
           >
             <icon-plus></icon-plus>
           </button>
